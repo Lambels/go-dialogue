3deff8cd8a2b3b10b698a6f436f482ef7ee3a245
Lambels <patrick.arvatu@yahoo.com>
2023-01-15 15:39:54 +0200

    refactored code

--- preamptive_reader.go
+++ preamptive_reader.go
@@ -7,19 +7,15 @@ import (
 	"sync/atomic"
 )
 
-// NewPreamptiveReader creates a new preamptive reader whith the provided context.
-//
-// If the context gets cancelled the caller waiting for the input will return early.
-//
-// IMPORTANT: the blocking read wont magically get cleaned up and will continue running till the read closes. You can still read from the
-// buffer which was allocated by the initiall call to Read (which got cancelled by the context) till the buffer is empty, calls to read after
-// the allocated buffer was emptied will all result in io.EOF .
+// NewPreamptiveReader creates a new preamptive reader with the provided context used to inidcate cancelation. The created
+// preamptive reader wraps and re directs reads to r.
 func NewPreamptiveReader(ctx context.Context, r io.Reader) *PreamptiveReader {
 	pr := &PreamptiveReader{
-		r:        r,
-		ctx:      ctx,
-		writeTo:  make(chan []byte),
-		readFrom: make(chan int, 1),
+		r:             r,
+		ctx:           ctx,
+		writeFromMem:  make(chan []byte),
+		writeFromRead: make(chan []byte),
+		bytesRead:     make(chan int),
 	}
 
 	go pr.listen()
@@ -27,7 +23,18 @@ func NewPreamptiveReader(ctx context.Context, r io.Reader) *PreamptiveReader {
 	return pr
 }
 
-// wrap around io.Reader for a work around read cancellations.
+// PreamptiveReader is a wrapper around r which provides cancellable reads. Reads are 1:1 up untill the context gets cancelled.
+// Calls to read after the context cancellation can block untill the previous read opperation is over. Once the read is over 
+// the previous buffer provided by the cancelled read call will be consumed on each subsequent read call till its all consumed and 
+// io.EOF is returned.
+//
+// IMPORTANT:
+//
+// Concurrent read calls will return an error.
+//
+// The read call to the source reader isnt cancelled itself, only the wrapped Read call returns early, the 
+// source read wont get cleaned up unitl the previous buffer is consumed.
+//
 // inspired by https://benjamincongdon.me/blog/2020/04/23/Cancelable-Reads-in-Go/
 type PreamptiveReader struct {
 	// the source reader.
@@ -41,31 +48,52 @@ type PreamptiveReader struct {
 	// this makes the process synchronous, one reader at a time, the rest will return an error.
 	claimRead atomic.Bool
 
-	buf      []byte      // refference to the current buffer provided by the call to Read().
-	writeTo  chan []byte // tells the reader we want to read.
-	readFrom chan int    // signals that the request read is over to whoever is intereseted.
-	err      error       // sticky error.
+	buf           []byte      // refference to the current buffer provided by the call to Read().
+	writeFromMem  chan []byte // reads from buf.
+	writeFromRead chan []byte // reads from the reader.
+	bytesRead     chan int    // signals that the request read is over to whoever is intereseted.
+	err           error       // sticky error.
 }
 
-// listen reads from r on demand.
+// listen reads from r or buf on demand.
 func (r *PreamptiveReader) listen() {
+	var n int
+	var err error
+
 	for {
 		select {
-		case <-r.ctx.Done():
-			r.err = r.ctx.Err()
-			return
-		case buf := <-r.writeTo: // commit to a read.
-			r.buf = buf // keep a refference to the buffer.
-			n, err := r.r.Read(buf)
-			if err != nil {
-				r.err = err
-				close(r.readFrom)
-				return
-			}
+		case buf := <-r.writeFromMem: // a read request on a closed context, try to read from the remaining buffer, this is quick.
+			n, err = r.readFromBuf(buf)
+		case buf := <-r.writeFromRead: // commit to a new to read.
+			n, err = r.r.Read(buf)
+			r.buf = buf
+		}
 
-			r.readFrom <- n
+		if err != nil {
+			r.err = err
+			close(r.bytesRead)
+			return
+		}
+
+		select {
+		case buf := <-r.writeFromMem: // got read from mem request druing the read call.
+			n, _ := r.readFromBuf(buf)
+			r.bytesRead <- n
+
+		case r.bytesRead <- n:
 		}
 	}
+}
+
+// readFromBuf copies the accumulated r.buf into buf and truncates r.buf. If len(r.buf) == 0 io.EOF is returned.
+func (r *PreamptiveReader) readFromBuf(buf []byte) (int, error) {
+	if len(r.buf) == 0 {
+		return 0, io.EOF
+	}
+
+	n := copy(buf, r.buf)
+	r.buf = r.buf[n:]
+	return n, nil
 }
 
 // Read reads requests a read from the preamptive reader. It behaves normally but returns early with n = 0 and the context error
@@ -74,64 +102,41 @@ func (r *PreamptiveReader) listen() {
 // The listen go routine wont get cleaned up till the stranded read returns, up until the cleanup, the first registered read will claim the
 // stranded read and work as expected.
 func (r *PreamptiveReader) Read(buf []byte) (int, error) {
-	fresh, err := r.claim()
-	if err != nil {
-		return 0, err
+	if !r.claimRead.CompareAndSwap(false, true) {
+		return 0, errors.New("cannot claim a read during another read")
 	}
 	defer r.claimRead.Store(false)
 
-	if fresh {
-		r.writeTo <- buf
-
-		select {
-		case <-r.ctx.Done():
-			return 0, r.ctx.Err()
-		case n, ok := <-r.readFrom:
-			if !ok {
-				return 0, r.err
-			}
-
-			return n, nil
+	// check wether we are calling after a cancelled context or before.
+	select {
+	case <-r.ctx.Done(): // call after cancelled context. Read only from memory, this opperation is usually fast excluding the times when we wait for an ongoing read.
+		// FASTPATH: buffer size is 0 and read is from memory, return early.
+		if len(buf) == 0 {
+			return 0, nil
 		}
+
+		r.writeFromMem <- buf
+
+		n, ok := <-r.bytesRead
+		if !ok {
+			return 0, r.err
+		}
+
+		return n, nil
+	default:
 	}
 
-	// FASTPATH: dont bother copying any values to a zero lenght buffer.
-	if len(buf) == 0 {
-		return 0, nil
-	}
-
-	// the read isnt fresh.
-	n, ok := <-r.readFrom
-	if !ok {
-		return 0, r.err
-	}
-
-	// copy values from previous buffer (provided by last read call) to current buffer
-	// while maintaining the readFrom state.
-	nn := copy(buf, r.buf[:n])
-
-	n -= nn
-	// last read, no more data left in the buffer.
-	if n == 0 {
-		close(r.readFrom)
-		r.err = io.EOF
-		return nn, nil
-	}
-
-	r.readFrom <- n
-	return nn, nil
-}
-
-// claim claims the preamptive reader and indicates if the claimed reader is new.
-func (r *PreamptiveReader) claim() (bool, error) {
-	if !r.claimRead.CompareAndSwap(false, true) {
-		return false, errors.New("cannot claim a read in process")
-	}
+	// context not cancelled and we claimed the reader. We need to request a new read.
+	r.writeFromRead <- buf
 
 	select {
 	case <-r.ctx.Done():
-		return false, nil
-	default:
-		return true, nil
+		return 0, r.ctx.Err()
+	case n, ok := <-r.bytesRead:
+		if !ok {
+			return 0, r.err
+		}
+
+		return n, nil
 	}
 }
