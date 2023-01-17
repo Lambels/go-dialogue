427bf576f80a6b74717d2e389fd9456592e4d34d
Lambels <patrick.arvatu@yahoo.com>
2023-01-17 22:10:52 +0200

    refactored all of dialogue + added preamptive reads

--- dialogue.go
+++ dialogue.go
@@ -3,195 +3,238 @@ package dialogue
 import (
 	"bufio"
 	"context"
+	"errors"
 	"io"
-	"net/http"
 	"strings"
 	"sync"
 )
 
+// Dialogue describes a back and forth discussion between the provided reader and writer.
 type Dialogue struct {
-	Prefix          string
-	R               io.Reader
-	W               io.Writer
-	NotFoundHandler func(context.Context, []string) error
-	HelpHandler     func([]*Command) string
+	// Prefix is an optional but recommended field which gets outputed before every read from R.
+	Prefix string
 
-	ctx    context.Context
-	cancel context.CancelFunc
+	// R is the source of the "conversation" where the user types in the message and the message is mapped to a command or
+	// to the CommandNotFound handler. R isnt used raw, its wrapped by the PreamptiveReader to provide preamptive reads which
+	// are cancellable by the base context.
+	R io.Reader
 
-	runningMu sync.Mutex
-	running   bool
-	done      chan struct{}
+	// W is the destination where the Prefix is written.
+	W io.Writer
 
-	// commands holds all the root commands.
-	commands   map[string]*Command
-	exchangeWg sync.WaitGroup
+	// CommandNotFound handels commands which arent mapped to anything. The ctx is the base context and the args are the full
+	// fields read from R including the command name.
+	//
+	// If nil the default CommandNotFound will be used which will call FormatHelp.
+	CommandNotFound func(ctx context.Context, args []string) error
+
+	// FormatHelp formats the help message for all the current commands. If left nil the default help formater will be used.
+	//
+	//TODO: Implement default command and format handler.
+	//TODO: Have specfic keywords for help.
+	//TODO: Have a flag set to parse help fields or pass that to the commands?
+	FormatHelp func([]*Command) string
+
+	// CommandContext optinally specifies a function to set the context for a command. The provided context is derived from the
+	// base context and its up to the implementation of the function to wrap or not the returned context with the base context
+	// but if not done no cancelation can be provided to the command.
+	CommandContext func(context.Context, string) context.Context
+
+	mu       sync.Mutex          // protects the fields below.
+	ctx      context.Context     // ctx is the base context used for cancelation.
+	cancel   context.CancelFunc  // cancel cancels the base context.
+	pr       *PreamptiveReader   // pr is the wrapped preamptive reader. (it is wrapped around R)
+	commands map[string]*Command // commands is a mapping of the command name to command.
+	running  bool                // indicates if the current dialogue is running.
+	close    chan struct{}
 }
 
-func NewDialogue(ctx context.Context, prefix string, r io.Reader, w io.Writer, notFound func(context.Context, []string) error) *Dialogue {
-	d := &Dialogue{
-		Prefix:          prefix,
-		R:               r,
-		W:               w,
-		ctx:             ctx,
-		NotFoundHandler: notFound,
-		done:            make(chan struct{}),
-		commands:        make(map[string]*Command),
-	}
-
-	if ctx == nil {
-		ctx = context.Background()
-	}
-	d.ctx, d.cancel = context.WithCancel(ctx)
-	d.ctx = context.WithValue(d.ctx, outKey{}, d.W)
-
-	return d
-}
-
-// Start starts the main processing thread where values from the dialogue are dispatched
-// to their own handlers.
+// Open initialises the dialogue and listens for tokens (provided by the default bufio.Scanner) and maps them to commands.
 //
-// It reports any error returned from the ReadWriter or from parsing the
-// dialogue.
-func (d *Dialogue) Start() error {
-	d.runningMu.Lock()
+// Open always returns non nil errors. After a call to Shutdown or Close the returned error is context.Canceled.
+//
+// IMPORTANT:
+//
+// You can open previously closed dialogues but be aware of the underlaying preamptive reader since it will always be binded to
+// the initiall reader and may read messages from the past transaction.
+func (d *Dialogue) Open() error {
+	d.mu.Lock()
 	d.running = true
-	d.runningMu.Unlock()
-	defer func() {
-		// free the done channel if any closer waiting.
+	d.initLocked()
+	d.mu.Unlock()
+
+	scanner := bufio.NewScanner(d.pr)
+	for {
+		// catch the close signal from the Shutdown call.
 		select {
-		case <-d.done:
+		case <-d.close:
+			return d.ctx.Err()
 		default:
 		}
 
-		d.runningMu.Lock()
-		d.running = false
-		d.runningMu.Unlock()
-	}()
+		if _, err := d.W.Write([]byte(d.Prefix)); err != nil {
+			return d.exit(err)
+		}
 
-	scan := bufio.NewScanner(d.R)
-	for {
-        d.exchangeWg.Add(1)
-		tkn, err := d.startExchange(scan)
+		advance := scanner.Scan()
+
+		if !advance {
+			return d.exit(scanner.Err())
+		}
+
+		token := scanner.Text()
+		fields := strings.Fields(token)
+
+		if len(fields) == 0 {
+			continue
+		}
+
+		err := d.dispatchHandler(fields[0], fields[1:])
 		if err != nil {
-            d.exchangeWg.Add(1)
-			return err
+			return d.exit(err)
 		}
-
-		cmd, args, err := parseRawCmd(tkn)
-		if err != nil {
-			d.exchangeWg.Done()
-			return err
-		}
-
-		if err := d.dispatchHandler(d.ctx, cmd, args); err != nil {
-			d.exchangeWg.Done()
-			return err
-		}
-		d.exchangeWg.Done()
 	}
 }
 
-// startExchange engages in a new exchange with the reader.
-func (d *Dialogue) startExchange(s *bufio.Scanner) ([]string, error) {
+// exit catches any close attempts and returns the context error or returns the provided error.
+func (d *Dialogue) exit(err error) error {
 	select {
-	case <-d.done:
-		return nil, io.EOF
+	case <-d.close:
+		return d.ctx.Err()
 	default:
 	}
 
-	if _, err := d.W.Write([]byte(d.Prefix)); err != nil {
-		return nil, err
+	d.mu.Lock()
+	d.running = false
+	d.mu.Unlock()
+
+	return err
+}
+
+func (d *Dialogue) initLocked() {
+	// check if context doesnt exist or previous context expired (this means the dialogue is being reused).
+	if d.ctx == nil || d.ctx.Err() != nil {
+		d.ctx, d.cancel = context.WithCancel(context.Background())
 	}
 
-	if !s.Scan() {
-		return nil, s.Err()
+	if d.close == nil {
+		d.close = make(chan struct{}, 1)
 	}
 
-	return strings.Fields(s.Text()), nil
+	if d.pr == nil {
+		d.pr = NewPreamptiveReader(d.ctx, d.R)
+	}
+}
+
+func (d *Dialogue) sendCloseNotify() <-chan struct{} {
+	notify := make(chan struct{}, 1)
+
+	go func() {
+		d.close <- struct{}{}
+		notify <- struct{}{}
+	}()
+
+	return notify
 }
 
 // dispatchHandler dispatches the handler for cmd if it exits or the not found handler.
 // finally it returns any error from the handlers.
-func (d *Dialogue) dispatchHandler(ctx context.Context, cmd string, args []string) error {
+func (d *Dialogue) dispatchHandler(cmd string, args []string) error {
 	command, ok := d.commands[cmd]
 	if !ok {
+		// accomodate the cmd name in the args to the not found handler.
 		tmp := make([]string, 1, len(args)+1)
 		tmp[0] = cmd
 		copy(tmp[1:], args)
 
-		return d.NotFoundHandler(ctx, tmp)
+		return d.CommandNotFound(d.ctx, tmp)
 	}
 
-	return command.parseAndRun(ctx, args)
+	cmdCtx := d.ctx
+	if cc := d.CommandContext; cc != nil {
+		cmdCtx = cc(cmdCtx, cmd)
+		if cmdCtx == nil {
+			return errors.New("CommandContext returned nil context")
+		}
+	}
+
+	return command.parseAndRun(cmdCtx, args)
 }
 
-// Stop stops the dialogue asap.
-//
-// If the dialogue is stuck on processing any value then its stopped immediately.
-//
-// If the dialogue is stuck on reading / writing to the ReadWriter,
-// its stopped as soon as a value goes through.
-func (d *Dialogue) Stop() {
-	d.runningMu.Lock()
-	defer d.runningMu.Unlock()
+// Close imidiately cancels the base context and always returns nil.
+func (d *Dialogue) Close() error {
+	d.mu.Lock()
+	defer d.mu.Unlock()
 
 	if !d.running {
-		return
+		return nil
 	}
 
+	<-d.sendCloseNotify()
 	d.cancel()
-	d.done <- struct{}{}
 	d.running = false
+	return nil
 }
 
-// StopGracefully stops any future dialogue from happening whilst waiting
-// for the current dialogue to ensue.
-//
-// If no current dialogue is happening, the behaviour is the same as with Stop.
-func (d *Dialogue) StopGracefully(ctx context.Context) {
-	d.runningMu.Lock()
-	defer d.runningMu.Unlock()
+// Shutdown gracefully shuts down the dialogue waiting for the current Read() or Command.Exec() opperation
+// without any interuption. It waits indefenetly for the current transaction to finish or till the provided context
+// expires. When the context expires the underlaying context is cancelled and the rest of the opperation behaves like a normal
+// call to Close().
+func (d *Dialogue) Shutdown(ctx context.Context) error {
+	d.mu.Lock()
+	defer d.mu.Unlock()
 
 	if !d.running {
-		return
+		return nil
 	}
 
-	var wgDone sync.WaitGroup
-	wgDone.Add(1)
-	go func() { d.done <- struct{}{}; wgDone.Done() }()
+	notify := d.sendCloseNotify()
+
 	select {
+	case <-notify:
+		return nil
 	case <-ctx.Done():
-	case <-signalWg(&d.exchangeWg):
+		d.cancel()
+		<-notify
+		return ctx.Err()
 	}
-
-	d.cancel()
-	wgDone.Wait()
-	d.running = false
 }
 
-func signalWg(wg *sync.WaitGroup) <-chan struct{} {
-	ch := make(chan struct{}, 1)
-	go func() {
-		wg.Wait()
-		ch <- struct{}{}
-	}()
-	return ch
-}
-
-// Handle registers hand as a handler for val. If a handler is already registered
-// for val then the old value is replaced.
-//
-// Handle is no-op after the dialogue started.
+// RegisterCommands registers the provided commands to the dialogue. If the dialogue is running the call is no-op. RegisterCommands
+// can be called even after a call to Close() or Shutdown() as long as the dialogue isnt running.
 func (d *Dialogue) RegisterCommands(cmds ...*Command) {
-	d.runningMu.Lock()
-	defer d.runningMu.Unlock()
+	d.mu.Lock()
+	defer d.mu.Unlock()
 
 	if d.running {
 		return
 	}
 
+	if d.commands == nil {
+		d.commands = make(map[string]*Command)
+	}
+
 	for _, c := range cmds {
 		d.commands[c.Name] = c
 	}
+}
+
+// PreamptiveReader returns the underlaying preamptive reader used by the dialogue. The underlaying preamptive reader can be
+// used to drain any remaining read / buffer or to merge with other readers after closing a dialogue.
+//
+// If the dialogue is currently running the call is no-op.
+//
+// IMPORTANT:
+//
+// If you plan on re opening the dialogue make sure to be responsible to what calls you are making to the preamptive reader since
+// you can only have one read at a time on the preamptive reader and you can cause un intended errors.
+func (d *Dialogue) PreamptiveReader() *PreamptiveReader {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+
+	if d.running {
+		return nil
+	}
+
+	return d.pr
 }
