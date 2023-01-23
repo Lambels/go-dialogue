c4de212c39a1f69e25c68f677f3ed2adb3835b5f
Lambels <patrick.arvatu@yahoo.com>
2023-01-23 23:05:25 +0200

    fixed tests

--- dialogue_test.go
+++ dialogue_test.go
@@ -13,100 +13,117 @@ import (
 )
 
 func TestShutdownWaitCurrentTransaction(t *testing.T) {
-    t.Parallel()
+	t.Parallel()
 
 	msg := "transaction complete"
 	w := NewWriteExpected(t, []byte(msg))
 
 	d := &dialogue.Dialogue{
 		R: stallingReader{
-			r: strings.NewReader("idk what im typing here"),
+			r: strings.NewReader("idk what im typing here\n"),
 			d: 5 * time.Second,
 		},
 		W: w,
 		CommandNotFound: func(ctx context.Context, args []string) error {
-            _, err := w.Write([]byte(msg))
+			_, err := w.Write([]byte(msg))
 			return err
 		},
 	}
+	d.RegisterCommands(testCommand)
 
-    closed := notifyClose(d)
-    go d.Shutdown(context.Background())
-    timeStart := time.Now()
+	closed := notifyClose(d)
+	time.Sleep(1 * time.Second)
+	go d.Shutdown(context.Background())
+	timeStart := time.Now()
 
-    select {
-    case err := <-closed:
-        timeElapsed := timeStart.Sub(time.Now())
-        if timeElapsed < 4 * time.Second {
-            t.Fatal("expected dialogue to close after 4 seconds")
-        }
+	select {
+	case err := <-closed:
+		timeElapsed := time.Now().Sub(timeStart)
+		if timeElapsed < 4*time.Second {
+			t.Fatal("expected dialogue to close after 4 seconds")
+		}
 
-        if err != dialogue.ErrDialogueClosed {
-            t.Fatalf("expected error to be %v but got %v", dialogue.ErrDialogueClosed, err)
-        }
+		if err != dialogue.ErrDialogueClosed {
+			t.Fatalf("expected error to be %v but got %v", dialogue.ErrDialogueClosed, err)
+		}
 
-        // get the underlaying reader to check if there was only one call to read.
-        pr := d.PreamptiveReader()
-        if _, err := pr.Read(make([]byte, 1)); err != io.EOF { // use buffer w capacity 1 to advance the reader, preamptive reader ignores memory reads with buffer sizes = 0.
-            t.Fatal("expected only one call to read")
-        }
-
-    case <-time.After(6 * time.Second):
-        t.Fatal("expected dialogue to close before 6 seconds")
-    }
+		// get the underlaying reader to check if there was only one call to read.
+		pr := d.PreamptiveReader()
+		if _, err := pr.Read(make([]byte, 1)); err != io.EOF { // use buffer w capacity 1 to advance the reader, preamptive reader ignores memory reads with buffer sizes = 0.
+			t.Fatal("expected only one call to read")
+		}
+	case <-time.After(6 * time.Second):
+		t.Fatal("expected dialogue to close before 6 seconds")
+	}
 }
 
 func TestShutdownDeadline(t *testing.T) {
-    t.Parallel()
+	t.Parallel()
 
-    msg := []byte("I hate unit tests")
+	msg := []byte("I hate unit tests")
 
-    d := &dialogue.Dialogue{
-        R: stallingReader{
-            r: bytes.NewReader(msg),
-            d: 5 * time.Second,
-        },
-        W: nopReadWriter{},
-        CommandNotFound: func(ctx context.Context, args []string) error {
-            t.Fatal("command not expected to run")
-            return nil
-        },
-    }
+	d := &dialogue.Dialogue{
+		R: stallingReader{
+			r: bytes.NewReader(msg),
+			d: 5 * time.Second,
+		},
+		W: nopReadWriter{},
+		CommandNotFound: func(ctx context.Context, args []string) error {
+			t.Fatal("command not expected to run")
+			return nil
+		},
+	}
+	d.RegisterCommands(testCommand)
 
-    closed := notifyClose(d)
-    ctx, cancel := context.WithTimeout(context.Background(), 3 * time.Second)
-    defer cancel()
+	closed := notifyClose(d)
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
 
-    go d.Shutdown(ctx)
+	time.Sleep(1 * time.Second)
+	go d.Shutdown(ctx)
 
-    select {
-    case err := <-closed:
-        if err != dialogue.ErrDialogueClosed {
-            t.Fatalf("expected error to be %v but got %v", dialogue.ErrDialogueClosed, err)
-        }
+	select {
+	case err := <-closed:
+		if err != dialogue.ErrDialogueClosed {
+			t.Fatalf("expected error to be %v but got %v", dialogue.ErrDialogueClosed, err)
+		}
 
-        // take over the failed read to confirm it happened.
-        buf := make([]byte, len(msg))
-        pr := d.PreamptiveReader()
+		// take over the failed read to confirm it happened.
+		buf := make([]byte, len(msg))
+		pr := d.PreamptiveReader()
 
-        _, err = pr.Read(buf)
-        if err != nil {
-            t.Fatal(err)
-        }
+		_, err = pr.Read(buf)
+		if err != nil {
+			t.Fatal(err)
+		}
 
-        if !bytes.Equal(buf, msg) {
-            t.Fatalf("got unexpected read: %s != %s", buf, msg)
-        }
-    case <-time.After(4 * time.Second):
-        t.Fatal("expected dialogue to close before 4 seconds")
-    }
-}
-
-func TestClose(t *testing.T) {
+		if !bytes.Equal(buf, msg) {
+			t.Fatalf("got unexpected read: %s != %s", buf, msg)
+		}
+	case <-time.After(4 * time.Second):
+		t.Fatal("expected dialogue to close before 4 seconds")
+	}
 }
 
 func TestHelpCommand(t *testing.T) {
+	t.Run("focus", func(t *testing.T) {
+		// expect the output to be the focused format of the command.
+		w := NewWriteExpected(t, []byte(testCommand.FormatHelp(
+			testCommand,
+			true,
+		)))
 
+		d := &dialogue.Dialogue{
+			R:       strings.NewReader("help -n test\n"),
+			W:       w,
+			HelpCmd: "help",
+		}
+
+		d.Open()
+	})
+
+	t.Run("generic", func(t *testing.T) {
+	})
 }
 
 func TestDefaultCommandNotFound(t *testing.T) {
@@ -117,14 +134,14 @@ func TestReadAfterClose(t *testing.T) {
 }
 
 func notifyClose(d *dialogue.Dialogue) <-chan error {
-    errC := make(chan error, 1)
+	errC := make(chan error, 1)
 
-    go func() {
-        err := d.Open()
-        errC <- err
-    }()
+	go func() {
+		err := d.Open()
+		errC <- err
+	}()
 
-    return errC
+	return errC
 }
 
 type nopReadWriter struct{}
@@ -162,7 +179,7 @@ func (w *writeExpected) Write(p []byte) (int, error) {
 	for i, b := range p {
 		expectedB := w.expected[w.wX+i]
 		if b != expectedB {
-			err := fmt.Errorf("writeExpected: missmatched bytes at index %d %s != %s", w.wX+i, expectedB, b)
+			err := fmt.Errorf("writeExpected: missmatched bytes at index %d %v != %v", w.wX+i, expectedB, b)
 			w.t.Error(err.Error())
 
 			// the strings matched till i.
