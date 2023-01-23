0a41a921512b34209fc46e72fe070ef09e27c7c7
Lambels <patrick.arvatu@yahoo.com>
2023-01-23 14:42:50 +0200

    cleaned up test

--- 0_examples/echo/main.go
+++ 0_examples/echo/main.go
@@ -2,57 +2,67 @@ package main
 
 import (
 	"context"
-	"flag"
 	"log"
 	"os"
 	"os/signal"
 	"strings"
 	"time"
+    "flag"
+    "fmt"
 
 	"github.com/Lambels/go-dialogue"
 )
 
 func main() {
 	d := &dialogue.Dialogue{
-		Prefix: "(echo) ",
-		R:      os.Stdin,
-		W:      os.Stdout,
+		Prefix:  "(echo) ",
+		R:       os.Stdin,
+		W:       os.Stdout,
+		HelpCmd: "help", // will generate the help command for us, it will be accesible under the "help" keyword.
+	}
+
+	fs := flag.NewFlagSet("echo", flag.ContinueOnError)
+	// n will be set to the default value by the go-dialogue tool after each itteration of the
+	// repeat command by default, no need to reset the value yourself.
+	n := fs.Int("n", 1, "sets the number of repetitions of the output")
+
+	repeat := func(_ *dialogue.CallChain, args []string) error {
+		for i := 0; i < *n; i++ {
+			_, err := fmt.Fprintln(d.W, strings.Join(args, " "))
+			if err != nil {
+				return err
+			}
+		}
+
+		return nil
 	}
 
 	d.RegisterCommands(
 		&dialogue.Command{
-			Name:    "echo",
-			Exec:    exec,
+			Name:      "echo",
+			Structure: "echo [-n repeat] <args>",
+			HelpShort: "echo will repeat the args -n times",
+			HelpLong:  "echo takes in the provided arguments and writes them back -n times (defaults to 1) to the writer",
+			FlagSet:   fs,
+			Exec:      repeat,
 		},
 	)
 
 	c := make(chan os.Signal, 1)
-    signal.Notify(c, os.Interrupt)
+	signal.Notify(c, os.Interrupt)
 
-    go func() {
-        <-c
-        log.Println("gracefully shutting down dialogue with a 5 second timeout...")
-        
-        ctx, cancel := context.WithTimeout(context.Background(), 5 * time.Second)
-        defer cancel()
+	go func() {
+		<-c
+        log.Println()
+		log.Println("gracefully shutting down dialogue with a 5 second timeout...")
 
-        d.Shutdown(ctx)
-    }()
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
 
-    if err := d.Open(); err != nil {
-        log.Fatal(err)
-    }
+		d.Shutdown(ctx)
+	}()
+
+	if err := d.Open(); err != nil {
+		log.Fatal(err)
+	}
 }
-
-func exec(_ *dialogue.CallChain, args []string) error {
-    log.Println(strings.Join(args, " "))
-    return nil
-}
-
-
-
-
-
-
-
-
